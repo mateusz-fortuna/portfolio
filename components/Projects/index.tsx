052ebf74f71f00192f502ba8fa62e7e3b90bd022
mateusz-fortuna <mat.fortuna@wp.pl>
2022-07-27 13:28:40 +0200

    Create stylesheet for the projects section

--- components/Projects/index.tsx
+++ components/Projects/index.tsx
@@ -1,8 +1,9 @@
 import { content } from "../../public/content";
 import Project from "../Project";
+import styles from "../../styles/Projects.module.sass";
 
 const Projects = () => (
-  <section className="">
+  <section className={styles.projects}>
     {Object.entries(content.projects).map((entry) => (
       <Project key={entry[0]} {...entry[1]} />
     ))}
