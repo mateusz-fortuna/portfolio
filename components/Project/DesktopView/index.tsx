76887b24a73822c76e5ca019acdbc8c95c728fd0
mateusz-fortuna <mat.fortuna@wp.pl>
2022-07-29 15:54:39 +0200

    Make the layout horizontal for desktop devices

--- components/Project/DesktopView/index.tsx
+++ components/Project/DesktopView/index.tsx
@@ -1,8 +1,18 @@
 import { Content } from "../../../public/content";
+import { Props } from "..";
 import styles from "../../../styles/Projects.module.sass";
+import TextSection from "../../TextSection";
+import Button from "../Button";
+import ProjectThumbnail from "../../Image";
 
-const DesktopView = ({ image, section }: Content) => (
-  <div className={styles.projects__desktopView}></div>
+const DesktopView = ({ image, section, name }: Props) => (
+  <div className={styles.projects__desktopView}>
+    <div className={styles.projects__desktopView_textSectionWrapper}>
+      <TextSection {...section} />
+      <Button name={name} />
+    </div>
+    <ProjectThumbnail {...image} />
+  </div>
 );
 
 export default DesktopView;
