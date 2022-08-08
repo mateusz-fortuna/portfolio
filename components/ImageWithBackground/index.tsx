ecbd65c0f15cd432a70fe546fa712dcda8c94285
mateusz-fortuna <mat.fortuna@wp.pl>
2022-08-08 16:58:21 +0200

    Fix aspect ratio of images

--- components/ImageWithBackground/index.tsx
+++ components/ImageWithBackground/index.tsx
@@ -6,28 +6,21 @@ import styles from "../../styles/Image.module.sass";
 import variables from "../../styles/variables.module.sass";
 
 const ImageWithBackground = ({ src, alt, aspectRatio }: Props) => {
-  const isLandscapeDeviceOrientation = useIsLandscape();
+  const isDesktop = useIsLandscape();
 
-  const desktopContainerSize = {
-    height: `calc(${(aspectRatio.height * 100) / aspectRatio.width}vw - 2 * ${
-      variables.marginStandard
-    }`,
-    width: "100vw",
+  const desktopWrapperSize = {
+    height: `calc(100vh - 8 * ${variables.marginStandard}`,
+    width: `calc((100vh - 8 * ${variables.marginStandard}) * ${aspectRatio.width} / ${aspectRatio.height})`,
   } as CSSProperties;
-  const mobileContainerSize = {
-    height: `calc(${(aspectRatio.height * 100) / aspectRatio.width}vw - 2 * ${
-      variables.marginStandard
-    }`,
+  const mobileWrapperSize = {
+    height: `calc((100vw - 2 * ${variables.marginStandard}) * ${aspectRatio.width} / ${aspectRatio.height})`,
+    width: `calc((100vw - 2 * ${variables.marginStandard})`,
   } as CSSProperties;
 
   return (
     <div
       className={styles.image__wrapper}
-      style={
-        isLandscapeDeviceOrientation
-          ? desktopContainerSize
-          : mobileContainerSize
-      }
+      style={isDesktop ? desktopWrapperSize : mobileWrapperSize}
     >
       <Image src={src} alt={alt} width="100%" height="100%" layout="fill" />
     </div>
