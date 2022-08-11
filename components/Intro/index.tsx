abad215e78c44b57be6b980c77a1c446a0018729
mateusz-fortuna <mat.fortuna@wp.pl>
2022-08-11 05:11:39 +0200

    Fix layout according to a UI project

--- components/Intro/index.tsx
+++ components/Intro/index.tsx
@@ -1,19 +1,28 @@
+import { useIsLandscape } from "../../hooks/useIsLandscape";
 import { content } from "../../public/content";
 import styles from "../../styles/Intro.module.sass";
 import IntroImage from "../ImageWithBackground";
 import TextSection from "../TextSection";
 
 const Intro = () => {
+  const hasDeviceLandscapeOrientation = useIsLandscape();
   const { section, image } = content.intro;
+
+  const introImage = <IntroImage {...image} />;
+  const text = (
+    <TextSection
+      title={section.title}
+      subtitle={section.subtitle}
+      description={section.description}
+    />
+  );
+
   return (
     <section className={styles.intro} id={Object.keys(content)[0]}>
       <>
-        <IntroImage {...image} />
-        <TextSection
-          title={section.title}
-          subtitle={section.subtitle}
-          description={section.description}
-        />
+        {hasDeviceLandscapeOrientation
+          ? [text, introImage]
+          : [introImage, text]}
       </>
     </section>
   );
