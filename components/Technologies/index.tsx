96d4d1751cf8cd3e11c175f4ac9dadfc4e9c5f7a
mateusz-fortuna <mat.fortuna@wp.pl>
2022-08-31 14:13:42 +0200

    Assign jsx technologies mapping to variables

--- components/Technologies/index.tsx
+++ components/Technologies/index.tsx
@@ -16,23 +16,27 @@ type Props = {
 };
 
 const Technologies = ({ technologies, tools }: Props) => {
+  const logos = technologies.map((name) => (
+    <Logo technology={name} key={name} />
+  ));
+
+  const toolsList = (
+    <ol>
+      {tools.map((tool) => (
+        <li key={tool}>{tool}</li>
+      ))}
+    </ol>
+  );
+
   return (
     <div className={styles.caseStudy__technologies}>
       <h2>
         <AnimatedText>Technologies</AnimatedText>
       </h2>
-      <div className={styles.caseStudy__technologies_logos}>
-        {technologies.map((name) => (
-          <Logo technology={name} key={name} />
-        ))}
-      </div>
+      <div className={styles.caseStudy__technologies_logos}>{logos}</div>
       <div className={styles.caseStudy__technologies_tools}>
         <AnimatedText nth={1}>Other libraries and tools</AnimatedText>
-        <ol>
-          {tools.map((tool) => (
-            <li key={tool}>{tool}</li>
-          ))}
-        </ol>
+        {toolsList}
       </div>
     </div>
   );
